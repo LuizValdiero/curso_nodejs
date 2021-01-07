63ebf495c8702396f2a6a022c4b2bdd86a0a49d7
Luiz Valdiero <luiz.valdiero@hotmail.com>
2021-01-07 20:33:38 -0300

    fix: test passwordConfirmation provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -58,6 +58,6 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: password confirmation'))
+    expect(httpResponse.body).toEqual(new Error('Missing param: passwordConfirmation'))
   })
 })
