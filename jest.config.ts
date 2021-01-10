f3281fae662b87866f075ed76fdfac4cf21a1423
Luiz Valdiero <luiz.valdiero@hotmail.com>
2021-01-10 14:59:58 -0300

    chore: add jest configs for mongodb

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,3 @@
-import { builtinModules } from "module"
-
 module.exports = {
 //  clearMocks: true,
 //  collectCoverage: true,
@@ -10,6 +8,7 @@ module.exports = {
     '<rootDir>/src'
   ],
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
