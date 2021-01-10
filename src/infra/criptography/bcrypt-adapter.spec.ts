ea74017ba2958ed511c28af2e65d22a1818a2eca
Luiz Valdiero <luiz.valdiero@hotmail.com>
2021-01-10 14:35:51 -0300

    test: ensure EncryptAdapter return a hash on success

---
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,20 @@
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from './bcrypt-adapter'
+
+describe('Bcrypt Adapter', () => {
+  test('Should call bcrypt with correct values', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.encrypt('any_value')
+    expect(hashSpy).toHaveBeenLastCalledWith('any_value', salt)
+  })
+
+  test('Should return a hash on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    jest.spyOn(bcrypt, 'hash').mockResolvedValueOnce('hash_value')
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash_value')
+  })
+})
