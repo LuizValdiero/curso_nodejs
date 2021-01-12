f0f5b77c03f0e19f480e28c52c981e3253c12416
Luiz Valdiero <luiz.valdiero@hotmail.com>
2021-01-11 22:14:44 -0300

    chore: update coverage config

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,10 @@ module.exports = {
 //  clearMocks: true,
 //  collectCoverage: true,
 
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   roots: [
     '<rootDir>/src'
