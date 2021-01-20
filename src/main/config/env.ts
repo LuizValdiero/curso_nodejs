0d2e0bf5c9d0314b36b72b64a7a26a376bb2a411
Luiz Valdiero <luiz.valdiero@hotmail.com>
2021-01-19 21:39:38 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 import mongodbAtlas from './mongodbAtlas'
 export default {
   mongoUrl: process.env.MONGO_URL || mongodbAtlas(),
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'sDa4rwd*ds?='
 }
