bc6b60d793f260c8482623aa6ccb1c6fe032f6ad
Luiz Valdiero <luiz.valdiero@hotmail.com>
2021-02-01 23:10:09 -0300

    fix: remove default MONGO_DB variable env to deploy

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,5 @@
-import mongodbAtlas from './mongodbAtlas'
 export default {
-  mongoUrl: process.env.MONGO_URL || mongodbAtlas(),
+  mongoUrl: process.env.MONGO_URL || 'local DB',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'sDa4rwd*ds?='
 }
