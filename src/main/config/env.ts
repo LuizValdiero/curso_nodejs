ffb7573e143319a65691433bcc964b3517ece505
Luiz Valdiero <luiz.valdiero@hotmail.com>
2021-03-19 00:59:12 -0300

    chore: change dev script to use docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'local DB',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'sDa4rwd*ds?='
 }
